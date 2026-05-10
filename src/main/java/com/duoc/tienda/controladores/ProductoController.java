23fdc1dbaa357fa0007a67911eb6642daae52b11
Valeria Gutiérrez Aravena <valegutierrezaravena@gmail.com>
2026-05-10 12:39:11 -0400

    Update ProductoController.java

--- src/main/java/com/duoc/tienda/controladores/ProductoController.java
+++ src/main/java/com/duoc/tienda/controladores/ProductoController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/productos")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductoController {
 
     private final ProductoService productoService;
